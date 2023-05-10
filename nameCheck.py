5c9018d9d4bc2188b4a8911e867e586626d9a177
cletus <43190890+cletusdotpy@users.noreply.github.com>
2023-05-10 17:13:02 -0400

    Update nameCheck.py
    Comments for readability

--- nameCheck.py
+++ nameCheck.py
@@ -12,16 +12,19 @@ import winsound
 import usernameSelenium
 
 run = True
-
+# Run indefinitely until condiiton to send text
 while run:
+    # Don't think this actually needs the fields specified, but regardless this users endpoint gets 500requests/24hrs.
     url = f'https://api.twitter.com/2/users/by?usernames={user_id}&user.fields=created_at'
     headers = {
         'Authorization': f'Bearer {bearertoke}'
     }
 
+    # Capture response and create plaintext var
     responseReal = requests.get(url, headers=headers)
     user_data = json.loads(responseReal.text)
 
+    # 'errors' exists when requested user is not found. Possibly prone to false alarms, but we can't all be perfect
     if 'errors' in user_data:
         print("BAD RESPONSE GO GET EM TIGER")
         frequency = 1000
@@ -33,7 +36,8 @@ while run:
 
     else:
         print(user_data)
-
+        # Print plaintext because why not, then countdown from 173 (500 / 24).
+        # This timer can be decreased, but you will eventually hit a rate cap
         for remaining in range(173, 0, -1):
             sys.stdout.write("\r")
             sys.stdout.write("{:2d} seconds remaining.".format(remaining))
